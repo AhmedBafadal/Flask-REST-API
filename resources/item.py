b54d01bcf0bfa679d66a9a94791c39a102d38e6f
Ahmed Bafadal <a.m.bafadal@gmail.com>
2018-07-02 21:31:51 +0100

    Added import

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_claims
 from models.item import ItemModel
 
 
