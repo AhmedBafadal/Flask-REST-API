ea2b9046821f40a2fcf285d2ddf46ce1d58d2657
Ahmed Bafadal <a.m.bafadal@gmail.com>
2018-07-02 15:55:37 +0100

    Fixed GET requests by changing find_all

--- models/store.py
+++ models/store.py
@@ -21,7 +21,7 @@ class StoreModel(db.Model):
 
     @classmethod
     def find_all(cls):
-        cls.query.all()
+        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
