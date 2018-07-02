0db71c156c8211f576fbeadad1e328ef3e664779
Ahmed Bafadal <a.m.bafadal@gmail.com>
2018-07-02 16:48:53 +0100

    Changed requirements.txt

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ api = Api(app)
 jwt = JWTManager(app)
 
 api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
 
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
